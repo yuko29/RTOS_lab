2e70377714dec57237a823ddfb543f34d4ecfb49
yuko29 <yuko29.hu@gmail.com>
2023-04-25 14:11:11 +0800

    [Lab2 Demo pass]

---
+++ Lab2_board/Test/obj/hello_ucosii.d
@@ -0,0 +1,41 @@
+obj/hello_ucosii.o: hello_ucosii.c ../Test_bsp//HAL/inc/includes.h \
+  ../Test_bsp//HAL/inc/os_cpu.h ../Test_bsp//HAL/inc/sys/alt_irq.h \
+  ../Test_bsp//HAL/inc/nios2.h ../Test_bsp//HAL/inc/alt_types.h \
+  ../Test_bsp/system.h ../Test_bsp/linker.h \
+  ../Test_bsp//UCOSII/inc/os_cfg.h ../Test_bsp//HAL/inc/sys/alt_alarm.h \
+  ../Test_bsp//HAL/inc/sys/alt_llist.h \
+  ../Test_bsp//HAL/inc/priv/alt_alarm.h ../Test_bsp//HAL/inc/alt_types.h \
+  ../Test_bsp/system.h ../Test_bsp//UCOSII/inc/ucos_ii.h \
+  ../Test_bsp//UCOSII/inc/os_cfg.h ../Test_bsp//HAL/inc/os_cpu.h
+
+../Test_bsp//HAL/inc/includes.h:
+
+../Test_bsp//HAL/inc/os_cpu.h:
+
+../Test_bsp//HAL/inc/sys/alt_irq.h:
+
+../Test_bsp//HAL/inc/nios2.h:
+
+../Test_bsp//HAL/inc/alt_types.h:
+
+../Test_bsp/system.h:
+
+../Test_bsp/linker.h:
+
+../Test_bsp//UCOSII/inc/os_cfg.h:
+
+../Test_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Test_bsp//HAL/inc/sys/alt_llist.h:
+
+../Test_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Test_bsp//HAL/inc/alt_types.h:
+
+../Test_bsp/system.h:
+
+../Test_bsp//UCOSII/inc/ucos_ii.h:
+
+../Test_bsp//UCOSII/inc/os_cfg.h:
+
+../Test_bsp//HAL/inc/os_cpu.h:
