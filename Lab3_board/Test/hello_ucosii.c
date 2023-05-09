ff5ba5f89c43b6527536bd639a67a348ac300b46
yuko29 <yuko29.hu@gmail.com>
2023-05-09 14:27:58 +0800

    [Lab3] Demo pass

---
+++ Lab3_board/Test/hello_ucosii.c
@@ -0,0 +1,474 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                          (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
+*                                           All Rights Reserved
+*
+*                                               EXAMPLE #3
+*********************************************************************************************************
+*/
+
+#include "includes.h"
+
+/*
+*********************************************************************************************************
+*                                              CONSTANTS
+*********************************************************************************************************
+*/
+
+#define TASK_STK_SIZE 512 /* Size of each task's stacks (# of WORDs)       */
+
+#define TASK_START_ID 0 /* Application tasks                             */
+
+#define MSG_QUEUE_SIZE 20 /* Size of message queue used in example         */
+
+#ifndef CTXSW_MSG_BUF_SIZE
+#define CTXSW_MSG_BUF_SIZE 20
+#endif
+
+#define NUM_TASK 5
+
+#define TEST_SET_ID 2
+/*
+*********************************************************************************************************
+*                                             DATA TYPES
+*********************************************************************************************************
+*/
+
+// typedef struct {
+//     char    TaskName[30];
+//     INT16U  TaskCtr;
+//     INT16U  TaskExecTime;
+//     INT32U  TaskTotExecTime;
+// } TASK_USER_DATA;
+
+typedef struct
+{
+    int compTime;
+    int period;
+} TASK_INFO;
+
+/*
+*********************************************************************************************************
+*                                              VARIABLES
+*********************************************************************************************************
+*/
+
+OS_STK TaskStartStk[TASK_STK_SIZE];      /* Startup    task stack                         */
+OS_STK TaskStk[NUM_TASK][TASK_STK_SIZE]; /* Task stacks                                   */
+
+// TASK_USER_DATA  TaskUserData[7];
+
+OS_EVENT *MsgQueue;    /* Message queue pointer                         */
+void *MsgQueueTbl[20]; /* Storage for messages                          */
+char CtxSwMsgBuf[CTXSW_MSG_BUF_SIZE][50];
+int CtxSwMsgCursor = 0;
+int PrintCursor = 0;
+TASK_INFO TaskInfoBuf[NUM_TASK];
+
+OS_EVENT *R1;
+OS_EVENT *R2;
+
+/*
+*********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+void TaskStart(void *data); /* Function prototypes of tasks                  */
+static void TaskStartCreateTaskSet1(void);
+static void TaskStartCreateTaskSet2(void);
+// static void TaskStartDispInit(void);
+// static void TaskStartDisp(void);
+void Task1(void *data);
+void Task2(void *data);
+void Task3(void *data);
+// void DispTaskStat(INT8U id);
+void printCtxSwMessage();
+void TaskArgumentSetting();
+void PrintTasksInfo();
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                                  MAIN
+*********************************************************************************************************
+*/
+
+void main(void)
+{
+    INT8U err1;
+    INT8U err2;
+
+    OSInit(); /* Initialize uC/OS-II                      */
+
+//    PC_DOSSaveReturn(); /* Save environment to return to DOS        */
+//
+//    PC_VectSet(uCOS, OSCtxSw); /* Install uC/OS-II's context switch vector */
+//
+//    PC_ElapsedInit(); /* Initialized elapsed time measurement     */
+
+    // OSTaskCreate(TaskStart, (void *)0, &TaskStartStk[TASK_STK_SIZE - 1], 0);
+#if TEST_SET_ID == 1
+    TaskStartCreateTaskSet1();
+#else if TEST_SET_ID == 2
+    TaskStartCreateTaskSet2();
+#endif
+    R1 = OSMutexCreate(1, &err1);
+    R2 = OSMutexCreate(2, &err1);
+    OSStart(); /* Start multitasking                       */
+}
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                               STARTUP TASK
+*********************************************************************************************************
+*/
+
+// void TaskStart(void *pdata)
+// {
+// #if OS_CRITICAL_METHOD == 3 /* Allocate storage for CPU status register */
+//     OS_CPU_SR cpu_sr;
+// #endif
+//     INT16S key;
+//     int i=0;
+
+//     pdata = pdata; /* Prevent compiler warning                 */
+
+//     OS_ENTER_CRITICAL(); /* Install uC/OS-II's clock tick ISR        */
+//     PC_VectSet(0x08, OSTickISR);
+//     PC_SetTickRate(OS_TICKS_PER_SEC); /* Reprogram tick rate                      */
+
+//     OS_EXIT_CRITICAL();
+
+// #if TEST_SET_ID == 1
+//     TaskStartCreateTaskSet1();
+// #else if TEST_SET_ID == 2
+//     TaskStartCreateTaskSet2();
+// #endif
+//     OSTimeSet(0);
+
+//     for (;;)
+//     {
+//         // TaskStartDisp(); /* Update the display                       */
+//         printCtxSwMessage(); /* Print the log of context switch */
+
+//         if (PC_GetKey(&key))
+//         { /* See if key has been pressed              */
+//             if (key == 0x1B)
+//             {                   /* Yes, see if it's the ESCAPE key          */
+//                 PC_DOSReturn(); /* Yes, return to DOS                       */
+//             }
+//         }
+
+//         OSCtxSwCtr = 0;                                    /* Clear context switch counter             */
+//         OSTimeDlyHMSM(0, 0, 1, 0);                         /* Wait one second                          */
+//     }
+// }
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                      CREATE APPLICATION TASKS
+*********************************************************************************************************
+*/
+#if TEST_SET_ID == 1
+void TaskStartCreateTaskSet1(void)
+{
+    OSTaskCreate(Task1,
+                 (void *)0,
+                 &TaskStk[0][TASK_STK_SIZE - 1],
+                 (INT8U)(3));
+
+    OSTaskCreate(Task2,
+                 (void *)0,
+                 &TaskStk[1][TASK_STK_SIZE - 1],
+                 (INT8U)(4));
+
+    OSTaskCreate(Task3,
+                 (void *)0,
+                 &TaskStk[2][TASK_STK_SIZE - 1],
+                 (INT8U)(5));
+}
+#else if TEST_SET_ID == 2
+void TaskStartCreateTaskSet2(void)
+{
+    OSTaskCreate(Task1,
+                 (void *)0,
+                 &TaskStk[0][TASK_STK_SIZE - 1],
+                 (INT8U)(3));
+
+    OSTaskCreate(Task2,
+                 (void *)0,
+                 &TaskStk[1][TASK_STK_SIZE - 1],
+                 (INT8U)(4));
+}
+#endif
+/*$PAGE*/
+
+/*$PAGE*/
+/*
+*********************************************************************************************************
+*                                               TASK
+*********************************************************************************************************
+*/
+
+#if TEST_SET_ID == 1
+
+void Task1(void *pdata)
+{
+#if OS_CRITICAL_METHOD == 3                           /* Allocate storage for CPU status register      */
+    OS_CPU_SR  cpu_sr = 0;
+#endif
+    char *msg;
+    INT16S key;
+
+    int start;
+    int todelay;
+    INT8U err;
+
+    // TASK_INFO *taskInfo = (TASK_INFO *)pdata;
+//    OS_ENTER_CRITICAL();
+//    PC_VectSet(0x08, OSTickISR); /* Install uC/OS-II's clock tick ISR        */
+//    PC_SetTickRate(1u);          /* Reprogram tick rate                      */
+//    OS_EXIT_CRITICAL();
+
+    start = 8;
+    todelay = start - OSTime;
+    if (todelay > 0)
+        OSTimeDly(todelay);
+
+    for (;;)
+    {
+//        if (PC_GetKey(&key))
+//        { /* See if key has been pressed              */
+//            if (key == 0x1B)
+//            {                   /* Yes, see if it's the ESCAPE key          */
+//                PC_DOSReturn(); /* Yes, return to DOS                       */
+//            }
+//        }
+        printCtxSwMessage();
+
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPend(R1, 0, &err);   // Lock R1
+
+
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPend(R2, 0, &err);   // Lock R2
+
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPost(R2);            // Unlock R2
+        OSMutexPost(R1);            // Unlock R1
+
+        OSTimeDly(100);
+    }
+}
+
+void Task2(void *pdata)
+{
+    int start;
+    int todelay;
+    INT8U err;
+
+    // // TASK_INFO *taskInfo = (TASK_INFO *)pdata;
+
+    start = 4;
+    todelay = start - OSTime;
+    if(todelay > 0)
+        OSTimeDly(todelay);
+
+    for (;;)
+    {
+        OSTCBCur->compTime = 2;
+        while(OSTCBCur->compTime > 0);
+        OSMutexPend(R2,0,&err);
+
+        OSTCBCur->compTime = 4;
+        while(OSTCBCur->compTime > 0);
+        OSMutexPost(R2);
+
+       OSTimeDly(100);
+    }
+
+    // OSTaskDel(OS_PRIO_SELF);
+}
+
+void Task3(void *pdata)
+{
+    int start;
+    int todelay;
+    INT8U err;
+
+    // // TASK_INFO *taskInfo = (TASK_INFO *)pdata;
+
+    start = 0;
+    todelay = start - OSTime;
+    if(todelay > 0) OSTimeDly(todelay);
+
+    for (;;)
+    {
+        OSTCBCur->compTime = 2;
+        while(OSTCBCur->compTime > 0);
+        OSMutexPend(R1,0,&err);
+
+        OSTCBCur->compTime = 7;
+        while(OSTCBCur->compTime > 0);
+        OSMutexPost(R1);
+
+        OSTimeDly(100);
+
+    }
+
+    // OSTaskDel(OS_PRIO_SELF);
+}
+
+#else if TEST_SET_ID == 2
+
+void Task1(void *pdata)
+{
+    char *msg;
+    INT16S key;
+
+    int start;
+    int todelay;
+    INT8U err;
+
+    // TASK_INFO *taskInfo = (TASK_INFO *)pdata;
+//    OS_ENTER_CRITICAL();
+//    PC_VectSet(0x08, OSTickISR); /* Install uC/OS-II's clock tick ISR        */
+//    PC_SetTickRate(1u);          /* Reprogram tick rate                      */
+//    OS_EXIT_CRITICAL();
+
+    start = 5;
+    todelay = start - OSTime;
+    if (todelay > 0)
+        OSTimeDly(todelay);
+
+    for (;;)
+    {
+//        if (PC_GetKey(&key))
+//        { /* See if key has been pressed              */
+//            if (key == 0x1B)
+//            {                   /* Yes, see if it's the ESCAPE key          */
+//                PC_DOSReturn(); /* Yes, return to DOS                       */
+//            }
+//        }
+
+
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPend(R2, 0, &err);       // Lock R2
+
+
+        OSTCBCur->compTime = 3;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPend(R1, 0, &err);       // Lock R1
+
+        OSTCBCur->compTime = 3;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPost(R1);                // Unlock R1
+
+        OSTCBCur->compTime = 3;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPost(R2);                // Unlock R2
+
+        printCtxSwMessage();
+
+        OSTimeDly(100);
+    }
+}
+
+void Task2(void *pdata)
+{
+    int start;
+    int todelay;
+    INT8U err;
+
+    start = 0;
+    todelay = start - OSTime;
+    if (todelay > 0)
+        OSTimeDly(todelay);
+
+    for (;;)
+    {
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPend(R1, 0, &err);       // Lock R1
+
+
+        OSTCBCur->compTime = 6;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPend(R2, 0, &err);       // Lock R2
+
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPost(R2);                // Unlock R2
+
+        OSTCBCur->compTime = 2;
+        while (OSTCBCur->compTime > 0);
+        OSMutexPost(R1);                // Unlock R1
+
+        OSTimeDly(100);
+    }
+
+}
+#endif
+
+void printCtxSwMessage()
+{
+    // static int i = 0;
+
+    // printf("PrintCursor: %d\n", PrintCursor);
+    for (; PrintCursor < CtxSwMsgCursor; PrintCursor++)
+        printf("%s", CtxSwMsgBuf[PrintCursor]);
+
+    if (PrintCursor > CTXSW_MSG_BUF_SIZE)
+    {
+        while (1)
+            ;
+    }
+}
+
+//void TaskArgumentSetting(TASK_INFO *TaskInfoBuf)
+//{
+//    OS_TCB *ptcb;
+//    ptcb = OSTCBList;
+//    while (ptcb->OSTCBPrio == 1 || ptcb->OSTCBPrio == 2 || ptcb->OSTCBPrio == 3)
+//    {
+//        if (ptcb->OSTCBPrio == 1)
+//        {
+//            ptcb->compTime = TaskInfoBuf[1].compTime;
+//            ptcb->period = TaskInfoBuf[1].period;
+//            ptcb->deadline = TaskInfoBuf[1].period;
+//        }
+//        else if (ptcb->OSTCBPrio == 2)
+//        {
+//            ptcb->compTime = TaskInfoBuf[2].compTime;
+//            ptcb->period = TaskInfoBuf[2].period;
+//            ptcb->deadline = TaskInfoBuf[2].period;
+//        }
+//        else if (ptcb->OSTCBPrio == 3)
+//        {
+//            ptcb->compTime = TaskInfoBuf[3].compTime;
+//            ptcb->period = TaskInfoBuf[3].period;
+//            ptcb->deadline = TaskInfoBuf[3].period;
+//        }
+//        ptcb = ptcb->OSTCBNext;
+//    }
+//}
+
+//void PrintTasksInfo()
+//{
+//    OS_TCB *ptcb;
+//    ptcb = OSTCBList;
+//    while (ptcb->OSTCBPrio == 0 || ptcb->OSTCBPrio == 1 || ptcb->OSTCBPrio == 2 || ptcb->OSTCBPrio == 3)
+//    {
+//        printf("Task prio: %d, deadline: %d\n", ptcb->OSTCBPrio, ptcb->deadline);
+//        ptcb = ptcb->OSTCBNext;
+//    }
+//}
---
+++ Lab3_board/Test_bsp/system.h
@@ -0,0 +1,455 @@
+/*
+ * system.h - SOPC Builder system and BSP software package information
+ *
+ * Machine generated for CPU 'cpu' in SOPC Builder design 'CORE_SOPC'
+ * SOPC Builder design path: C:/Users/CPS/Downloads/CORE_SOPC.sopcinfo
+ *
+ * Generated: Thu Mar 01 18:02:08 CST 2018
+ */
+
+/*
+ * DO NOT MODIFY THIS FILE
+ *
+ * Changing this file will have subtle consequences
+ * which will almost certainly lead to a nonfunctioning
+ * system. If you do modify this file, be aware that your
+ * changes will be overwritten and lost when this file
+ * is generated again.
+ *
+ * DO NOT MODIFY THIS FILE
+ */
+
+/*
+ * License Agreement
+ *
+ * Copyright (c) 2008
+ * Altera Corporation, San Jose, California, USA.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * This agreement shall be governed in all respects by the laws of the State
+ * of California and by the laws of the United States of America.
+ */
+
+#ifndef __SYSTEM_H_
+#define __SYSTEM_H_
+
+/* Include definitions from linker script generator */
+#include "linker.h"
+
+
+/*
+ * CPU configuration
+ *
+ */
+
+#define ALT_CPU_ARCHITECTURE "altera_nios2"
+#define ALT_CPU_BIG_ENDIAN 0
+#define ALT_CPU_BREAK_ADDR 0x1020
+#define ALT_CPU_CPU_FREQ 50000000u
+#define ALT_CPU_CPU_ID_SIZE 1
+#define ALT_CPU_CPU_ID_VALUE 0x0
+#define ALT_CPU_CPU_IMPLEMENTATION "fast"
+#define ALT_CPU_DATA_ADDR_WIDTH 0x1b
+#define ALT_CPU_DCACHE_LINE_SIZE 0
+#define ALT_CPU_DCACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_DCACHE_SIZE 0
+#define ALT_CPU_EXCEPTION_ADDR 0x4000020
+#define ALT_CPU_FLUSHDA_SUPPORTED
+#define ALT_CPU_FREQ 50000000
+#define ALT_CPU_HARDWARE_DIVIDE_PRESENT 0
+#define ALT_CPU_HARDWARE_MULTIPLY_PRESENT 1
+#define ALT_CPU_HARDWARE_MULX_PRESENT 0
+#define ALT_CPU_HAS_DEBUG_CORE 1
+#define ALT_CPU_HAS_DEBUG_STUB
+#define ALT_CPU_HAS_JMPI_INSTRUCTION
+#define ALT_CPU_ICACHE_LINE_SIZE 32
+#define ALT_CPU_ICACHE_LINE_SIZE_LOG2 5
+#define ALT_CPU_ICACHE_SIZE 2048
+#define ALT_CPU_INST_ADDR_WIDTH 0x1b
+#define ALT_CPU_NAME "cpu"
+#define ALT_CPU_NUM_OF_SHADOW_REG_SETS 0
+#define ALT_CPU_RESET_ADDR 0x0
+
+
+/*
+ * CPU configuration (with legacy prefix - don't use these anymore)
+ *
+ */
+
+#define NIOS2_BIG_ENDIAN 0
+#define NIOS2_BREAK_ADDR 0x1020
+#define NIOS2_CPU_FREQ 50000000u
+#define NIOS2_CPU_ID_SIZE 1
+#define NIOS2_CPU_ID_VALUE 0x0
+#define NIOS2_CPU_IMPLEMENTATION "fast"
+#define NIOS2_DATA_ADDR_WIDTH 0x1b
+#define NIOS2_DCACHE_LINE_SIZE 0
+#define NIOS2_DCACHE_LINE_SIZE_LOG2 0
+#define NIOS2_DCACHE_SIZE 0
+#define NIOS2_EXCEPTION_ADDR 0x4000020
+#define NIOS2_FLUSHDA_SUPPORTED
+#define NIOS2_HARDWARE_DIVIDE_PRESENT 0
+#define NIOS2_HARDWARE_MULTIPLY_PRESENT 1
+#define NIOS2_HARDWARE_MULX_PRESENT 0
+#define NIOS2_HAS_DEBUG_CORE 1
+#define NIOS2_HAS_DEBUG_STUB
+#define NIOS2_HAS_JMPI_INSTRUCTION
+#define NIOS2_ICACHE_LINE_SIZE 32
+#define NIOS2_ICACHE_LINE_SIZE_LOG2 5
+#define NIOS2_ICACHE_SIZE 2048
+#define NIOS2_INST_ADDR_WIDTH 0x1b
+#define NIOS2_NUM_OF_SHADOW_REG_SETS 0
+#define NIOS2_RESET_ADDR 0x0
+
+
+/*
+ * Define for each module class mastered by the CPU
+ *
+ */
+
+#define __ALTERA_AVALON_EPCS_FLASH_CONTROLLER
+#define __ALTERA_AVALON_JTAG_UART
+#define __ALTERA_AVALON_NEW_SDRAM_CONTROLLER
+#define __ALTERA_AVALON_PIO
+#define __ALTERA_AVALON_SYSID
+#define __ALTERA_AVALON_TIMER
+#define __ALTERA_NIOS2
+
+
+/*
+ * LED configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_LED altera_avalon_pio
+#define LED_BASE 0x1820
+#define LED_BIT_CLEARING_EDGE_REGISTER 0
+#define LED_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define LED_CAPTURE 0
+#define LED_DATA_WIDTH 4
+#define LED_DO_TEST_BENCH_WIRING 0
+#define LED_DRIVEN_SIM_VALUE 0x0
+#define LED_EDGE_TYPE "NONE"
+#define LED_FREQ 50000000u
+#define LED_HAS_IN 0
+#define LED_HAS_OUT 1
+#define LED_HAS_TRI 0
+#define LED_IRQ -1
+#define LED_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define LED_IRQ_TYPE "NONE"
+#define LED_NAME "/dev/LED"
+#define LED_RESET_VALUE 0x0
+#define LED_SPAN 16
+#define LED_TYPE "altera_avalon_pio"
+
+
+/*
+ * System configuration
+ *
+ */
+
+#define ALT_DEVICE_FAMILY "CYCLONEIVE"
+#define ALT_ENHANCED_INTERRUPT_API_PRESENT
+#define ALT_IRQ_BASE NULL
+#define ALT_LOG_PORT "/dev/null"
+#define ALT_LOG_PORT_BASE 0x0
+#define ALT_LOG_PORT_DEV null
+#define ALT_LOG_PORT_TYPE ""
+#define ALT_NUM_EXTERNAL_INTERRUPT_CONTROLLERS 0
+#define ALT_NUM_INTERNAL_INTERRUPT_CONTROLLERS 1
+#define ALT_NUM_INTERRUPT_CONTROLLERS 1
+#define ALT_STDERR "/dev/jtag_uart"
+#define ALT_STDERR_BASE 0x1858
+#define ALT_STDERR_DEV jtag_uart
+#define ALT_STDERR_IS_JTAG_UART
+#define ALT_STDERR_PRESENT
+#define ALT_STDERR_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDIN "/dev/jtag_uart"
+#define ALT_STDIN_BASE 0x1858
+#define ALT_STDIN_DEV jtag_uart
+#define ALT_STDIN_IS_JTAG_UART
+#define ALT_STDIN_PRESENT
+#define ALT_STDIN_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDOUT "/dev/jtag_uart"
+#define ALT_STDOUT_BASE 0x1858
+#define ALT_STDOUT_DEV jtag_uart
+#define ALT_STDOUT_IS_JTAG_UART
+#define ALT_STDOUT_PRESENT
+#define ALT_STDOUT_TYPE "altera_avalon_jtag_uart"
+#define ALT_SYSTEM_NAME "CORE_SOPC"
+
+
+/*
+ * col configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_col altera_avalon_pio
+#define COL_BASE 0x1830
+#define COL_BIT_CLEARING_EDGE_REGISTER 0
+#define COL_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define COL_CAPTURE 0
+#define COL_DATA_WIDTH 4
+#define COL_DO_TEST_BENCH_WIRING 0
+#define COL_DRIVEN_SIM_VALUE 0x0
+#define COL_EDGE_TYPE "NONE"
+#define COL_FREQ 50000000u
+#define COL_HAS_IN 0
+#define COL_HAS_OUT 0
+#define COL_HAS_TRI 1
+#define COL_IRQ -1
+#define COL_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define COL_IRQ_TYPE "NONE"
+#define COL_NAME "/dev/col"
+#define COL_RESET_VALUE 0x0
+#define COL_SPAN 16
+#define COL_TYPE "altera_avalon_pio"
+
+
+/*
+ * epcs configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_epcs altera_avalon_epcs_flash_controller
+#define EPCS_BASE 0x0
+#define EPCS_IRQ 0
+#define EPCS_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define EPCS_NAME "/dev/epcs"
+#define EPCS_REGISTER_OFFSET 1024
+#define EPCS_SPAN 2048
+#define EPCS_TYPE "altera_avalon_epcs_flash_controller"
+
+
+/*
+ * hal configuration
+ *
+ */
+
+#define ALT_MAX_FD 32
+#define ALT_SYS_CLK UCOSII_TIMER
+#define ALT_TIMESTAMP_CLK none
+
+
+/*
+ * jtag_uart configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_jtag_uart altera_avalon_jtag_uart
+#define JTAG_UART_BASE 0x1858
+#define JTAG_UART_IRQ 1
+#define JTAG_UART_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define JTAG_UART_NAME "/dev/jtag_uart"
+#define JTAG_UART_READ_DEPTH 64
+#define JTAG_UART_READ_THRESHOLD 8
+#define JTAG_UART_SPAN 8
+#define JTAG_UART_TYPE "altera_avalon_jtag_uart"
+#define JTAG_UART_WRITE_DEPTH 64
+#define JTAG_UART_WRITE_THRESHOLD 8
+
+
+/*
+ * row configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_row altera_avalon_pio
+#define ROW_BASE 0x1840
+#define ROW_BIT_CLEARING_EDGE_REGISTER 0
+#define ROW_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define ROW_CAPTURE 0
+#define ROW_DATA_WIDTH 4
+#define ROW_DO_TEST_BENCH_WIRING 0
+#define ROW_DRIVEN_SIM_VALUE 0x0
+#define ROW_EDGE_TYPE "NONE"
+#define ROW_FREQ 50000000u
+#define ROW_HAS_IN 0
+#define ROW_HAS_OUT 0
+#define ROW_HAS_TRI 1
+#define ROW_IRQ -1
+#define ROW_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ROW_IRQ_TYPE "NONE"
+#define ROW_NAME "/dev/row"
+#define ROW_RESET_VALUE 0x0
+#define ROW_SPAN 16
+#define ROW_TYPE "altera_avalon_pio"
+
+
+/*
+ * sdram configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sdram altera_avalon_new_sdram_controller
+#define SDRAM_BASE 0x4000000
+#define SDRAM_CAS_LATENCY 3
+#define SDRAM_CONTENTS_INFO ""
+#define SDRAM_INIT_NOP_DELAY 0.0
+#define SDRAM_INIT_REFRESH_COMMANDS 2
+#define SDRAM_IRQ -1
+#define SDRAM_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SDRAM_IS_INITIALIZED 1
+#define SDRAM_NAME "/dev/sdram"
+#define SDRAM_POWERUP_DELAY 100.0
+#define SDRAM_REFRESH_PERIOD 15.625
+#define SDRAM_REGISTER_DATA_IN 1
+#define SDRAM_SDRAM_ADDR_WIDTH 0x18
+#define SDRAM_SDRAM_BANK_WIDTH 2
+#define SDRAM_SDRAM_COL_WIDTH 9
+#define SDRAM_SDRAM_DATA_WIDTH 16
+#define SDRAM_SDRAM_NUM_BANKS 4
+#define SDRAM_SDRAM_NUM_CHIPSELECTS 1
+#define SDRAM_SDRAM_ROW_WIDTH 13
+#define SDRAM_SHARED_DATA 0
+#define SDRAM_SIM_MODEL_BASE 0
+#define SDRAM_SPAN 33554432
+#define SDRAM_STARVATION_INDICATOR 0
+#define SDRAM_TRISTATE_BRIDGE_SLAVE ""
+#define SDRAM_TYPE "altera_avalon_new_sdram_controller"
+#define SDRAM_T_AC 5.5
+#define SDRAM_T_MRD 3
+#define SDRAM_T_RCD 20.0
+#define SDRAM_T_RFC 70.0
+#define SDRAM_T_RP 20.0
+#define SDRAM_T_WR 14.0
+
+
+/*
+ * sysid configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sysid altera_avalon_sysid
+#define SYSID_BASE 0x1850
+#define SYSID_ID 0u
+#define SYSID_IRQ -1
+#define SYSID_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SYSID_NAME "/dev/sysid"
+#define SYSID_SPAN 8
+#define SYSID_TIMESTAMP 1373197755u
+#define SYSID_TYPE "altera_avalon_sysid"
+
+
+/*
+ * ucosii configuration
+ *
+ */
+
+#define OS_ARG_CHK_EN 1
+#define OS_CPU_HOOKS_EN 1
+#define OS_DEBUG_EN 1
+#define OS_EVENT_NAME_SIZE 32
+#define OS_FLAGS_NBITS 16
+#define OS_FLAG_ACCEPT_EN 1
+#define OS_FLAG_DEL_EN 1
+#define OS_FLAG_EN 1
+#define OS_FLAG_NAME_SIZE 32
+#define OS_FLAG_QUERY_EN 1
+#define OS_FLAG_WAIT_CLR_EN 1
+#define OS_LOWEST_PRIO 20
+#define OS_MAX_EVENTS 60
+#define OS_MAX_FLAGS 20
+#define OS_MAX_MEM_PART 60
+#define OS_MAX_QS 20
+#define OS_MAX_TASKS 10
+#define OS_MBOX_ACCEPT_EN 1
+#define OS_MBOX_DEL_EN 1
+#define OS_MBOX_EN 1
+#define OS_MBOX_POST_EN 1
+#define OS_MBOX_POST_OPT_EN 1
+#define OS_MBOX_QUERY_EN 1
+#define OS_MEM_EN 1
+#define OS_MEM_NAME_SIZE 32
+#define OS_MEM_QUERY_EN 1
+#define OS_MUTEX_ACCEPT_EN 1
+#define OS_MUTEX_DEL_EN 1
+#define OS_MUTEX_EN 1
+#define OS_MUTEX_QUERY_EN 1
+#define OS_Q_ACCEPT_EN 1
+#define OS_Q_DEL_EN 1
+#define OS_Q_EN 1
+#define OS_Q_FLUSH_EN 1
+#define OS_Q_POST_EN 1
+#define OS_Q_POST_FRONT_EN 1
+#define OS_Q_POST_OPT_EN 1
+#define OS_Q_QUERY_EN 1
+#define OS_SCHED_LOCK_EN 1
+#define OS_SEM_ACCEPT_EN 1
+#define OS_SEM_DEL_EN 1
+#define OS_SEM_EN 1
+#define OS_SEM_QUERY_EN 1
+#define OS_SEM_SET_EN 1
+#define OS_TASK_CHANGE_PRIO_EN 1
+#define OS_TASK_CREATE_EN 1
+#define OS_TASK_CREATE_EXT_EN 1
+#define OS_TASK_DEL_EN 1
+#define OS_TASK_IDLE_STK_SIZE 512
+#define OS_TASK_NAME_SIZE 32
+#define OS_TASK_PROFILE_EN 1
+#define OS_TASK_QUERY_EN 1
+#define OS_TASK_STAT_EN 1
+#define OS_TASK_STAT_STK_CHK_EN 1
+#define OS_TASK_STAT_STK_SIZE 512
+#define OS_TASK_SUSPEND_EN 1
+#define OS_TASK_SW_HOOK_EN 1
+#define OS_TASK_TMR_PRIO 0
+#define OS_TASK_TMR_STK_SIZE 512
+#define OS_THREAD_SAFE_NEWLIB 1
+#define OS_TICKS_PER_SEC UCOSII_TIMER_TICKS_PER_SEC
+#define OS_TICK_STEP_EN 1
+#define OS_TIME_DLY_HMSM_EN 1
+#define OS_TIME_DLY_RESUME_EN 1
+#define OS_TIME_GET_SET_EN 1
+#define OS_TIME_TICK_HOOK_EN 1
+#define OS_TMR_CFG_MAX 16
+#define OS_TMR_CFG_NAME_SIZE 16
+#define OS_TMR_CFG_TICKS_PER_SEC 10
+#define OS_TMR_CFG_WHEEL_SIZE 2
+#define OS_TMR_EN 0
+
+
+/*
+ * ucosii_timer configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_ucosii_timer altera_avalon_timer
+#define UCOSII_TIMER_ALWAYS_RUN 0
+#define UCOSII_TIMER_BASE 0x1800
+#define UCOSII_TIMER_COUNTER_SIZE 32
+#define UCOSII_TIMER_FIXED_PERIOD 0
+#define UCOSII_TIMER_FREQ 50000000u
+#define UCOSII_TIMER_IRQ 2
+#define UCOSII_TIMER_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define UCOSII_TIMER_LOAD_VALUE 499999ull
+#define UCOSII_TIMER_MULT 0.0010
+#define UCOSII_TIMER_NAME "/dev/ucosii_timer"
+#define UCOSII_TIMER_PERIOD 10
+#define UCOSII_TIMER_PERIOD_UNITS "ms"
+#define UCOSII_TIMER_RESET_OUTPUT 0
+#define UCOSII_TIMER_SNAPSHOT 1
+#define UCOSII_TIMER_SPAN 32
+#define UCOSII_TIMER_TICKS_PER_SEC 100u
+#define UCOSII_TIMER_TIMEOUT_PULSE_OUTPUT 0
+#define UCOSII_TIMER_TYPE "altera_avalon_timer"
+
+#endif /* __SYSTEM_H_ */
